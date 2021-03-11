a89e5a6ea60aa1e533405e35faef9766f346ddaa
CaptainCandy <33003223+CaptainCandy@users.noreply.github.com>
2021-03-11 10:39:41 +0800

    reviews statistics for datasets.

--- model/test.py
+++ model/test.py
@@ -1,40 +1,63 @@
-import gensim
-import numpy as np
-import matplotlib.pyplot as plt
+import wordcloud
 
-# %%
-# f = open("../data/GoogleNews-vectors-negative300.txt", "r")
+# s = "Seen through the eyes of a squad of American soldiers, the story begins with World War II's historic Omaha Beach D-Day invasion, then moves beyond the beach as the men embark on a dangerous special mission. Captain John Miller (Hanks) must take his men behind enemy lines to find Private James Ryan, whose three brothers have been killed in combat. Face with impossible odds, the men question their orders. Why are eight men risking their lives to save just one? Surrounded by the brutal realities of war, each man searches for his own answer -- the strength to triumph over an uncertain future with honor, decency and courage."
+# r = "12 Angry Men, by Sidney Lumet, may be the most radical big-screen courtroom drama in cinema history. A behind-closed-doors look at the American legal system as riveting as it is spare, the iconic adaptation of Reginald Rose’s teleplay stars Henry Fonda as the initially dissenting member of a jury of white men ready to pass judgment on a Puerto Rican teenager charged with murdering his father. What results is a saga of epic proportions that plays out in real time over ninety minutes in one sweltering room. Lumet’s electrifying snapshot of 1950s America on the verge of change is one of the great feature-film debuts."
+# r = "Eleven jurors are convinced that the defendant is guilty of murder. The twelfth has no doubt of his innocence. How can this one man steer the others toward the same conclusion? It's a case of seemingly overwhelming evidence against a teenager accused of killing his father in 'one of the best pictures ever made'"
+# t = "The next instalment in the 'Star Wars' franchise. Rebel Luke Skywalker (Mark Hamill) and his friends continue to battle evil in the form of the decadent galactic empire, headed by Jedi-gone-bad Darth Vader (Dave Prowse, with the voice of James Earl Jones), as the ruthless Palpatine (Ian McDiarmid) sets plans in motion to build a second Death Star with the purpose of destroying the Rebel Alliance."
+#
+# w = wordcloud.WordCloud(width=800, height=400, background_color='white')
+# w.generate(s)
+# w.to_file("../example/ryan.jpg")
+# w.generate(r)
+# w.to_file("../example/angry.jpg")
+# w.generate(t)
+# w.to_file("../example/sw.jpg")
 
-# model = gensim.models.KeyedVectors.load_word2vec_format("../data/GoogleNews-vectors-negative300.bin", binary=True)
+#%%
+import dill as pickle
+import pandas as pd
+import re
 
-# model.wv.save_word2vec_format("../data/GoogleNews-vectors-negative300.txt")
 
-# %%
-# with open("../data/instruments/Musical_Instruments_5.json", "r") as f:
-#     lines = f.readlines()
-#     # print(lines)
-# with open("../data/instruments/Musical_Instruments_5_new.json", "w", encoding="utf-8") as f_new:
-#     for line in lines:
-#         if "reviewText" not in line:
-#             continue
-#         f_new.write(line)
+def clean_str(string):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    """
+    string = re.sub(r"[^A-Za-z]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
 
-# %%
-criterion = np.load("./criterion_2021-02-08_22h47m03s.npz")
-rmse_train, rmse_test, mae_train, mae_test = criterion["rmse_train"], criterion["rmse_test"], \
-                                             criterion["mae_train"], criterion["mae_test"]
-# 绘制曲线
-print(np.min(mae_test))
-print(np.min(rmse_test))
-plt.figure(figsize=(15, 6))
-plt.subplot(121)
-plt.plot(rmse_train)
-plt.plot(rmse_test)
-plt.legend(('Train rmse', 'Val rmse'))
-plt.title("RMSE")
-plt.subplot(122)
-plt.plot(mae_train)
-plt.plot(mae_test)
-plt.title("MAE")
-plt.legend(('Train mae', 'Val mae'))
-plt.savefig('./results/%s.jpg' % "instruments_20210208")
+
+dataset_name = "music"
+# reviews_all = []
+reviews_all_len = []
+with open("../data/%s_bert/reviews_all" % dataset_name, "rb") as f:
+    while True:
+        try:
+            line = pickle.load(f)
+            line = clean_str(line)
+            line = line.split(" ")
+            # reviews_all.append(line)
+            reviews_all_len.append(len(line))
+        except EOFError:
+            break
+r = pd.Series(reviews_all_len)
+print(r.describe())
+
+count = 0
+with open("../data/%s/vocabulary_all.txt" % dataset_name, "r") as f:
+    for line in f:
+        count += 1
+print(count)
